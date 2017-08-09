e81cd98d1c1b9cd8304b74e24b398dfe4c86c5c3
Laura Caroselli <lcaroselli@outlook.com>
2017-08-08 22:11:59 -0600

    Add Redux Actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,26 @@
+//increment action to increment a like
+export function increment(index) {
+  return {
+    type: 'INCREMENT_LIKES', //type is INCREMENT_LIKES
+    index //same as index: index, payload is index
+  }
+}
+
+//add comment
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT', //type is ADD_COMMENT
+    postId,
+    author,
+    comment
+  } //payload is postId, author, and comment
+}
+
+//remove comment
+export function removeComment(postId, index) {
+  return {
+    type: 'REMOVE_COMMENT',
+    postId,
+    index
+  }
+}
