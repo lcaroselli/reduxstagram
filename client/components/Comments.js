c9d38ca4107796a10776fc21f2a167e8c23fd069
Laura Caroselli <lcaroselli@outlook.com>
2017-08-11 14:39:17 -0600

    Add and remove comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ export default class Comments extends React.Component {
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -19,6 +19,7 @@ export default class Comments extends React.Component {
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   }
 
   render() {
